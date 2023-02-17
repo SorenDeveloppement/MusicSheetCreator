8edd58fb7628f5eadc7f54fd00c528246cbe55d1
SorenDeveloppement <sorenel@yahoo.com>
2023-02-17 23:12:55 +0100

    End of init args of SheetCreator class

--- SheetCreator.py
+++ SheetCreator.py
@@ -2,5 +2,5 @@ import SheetProperties
 
 
 class SheetCreator:
-    def __init__(self, speed: int, key: SheetProperties, accidental_nb: int, accidental_type: SheetProperties):
+    def __init__(self, title: str, subtitle: str | None, author: str | None, instrument: str | None, speed: int, key: SheetProperties, accidental_nb: int, accidental_type: SheetProperties):
         ...
